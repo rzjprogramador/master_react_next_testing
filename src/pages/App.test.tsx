9ab698f98acfb06eb51ce302ee9eb4525093db44
rzjprogramador <rzjprogramador@gmail.com>
2022-10-24 15:19:44 -0300

    chore: inicio

---
+++ src/pages/App.test.tsx
@@ -0,0 +1,14 @@
+import "@testing-library/jest-dom"
+import { render } from "@testing-library/react"
+import { describe, expect, test } from "vitest"
+
+import App from "@src/pages/_app"
+
+describe("App", () => {
+  test("deve renderizar o App com o texto: Alo mundo", () => {
+    const { debug, getByText } = render(<App />)
+    // debug()
+
+    expect(getByText("Alo mundo")).toBeInTheDocument()
+  })
+})
---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>Alo mundo</h1>
+    </>
+  )
+}
+
+export default App
