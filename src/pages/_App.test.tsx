8e29b65f08c1d47eaff2a4e354f92e3123ac36e3
rzjprogramador <rzjprogramador@gmail.com>
2022-10-24 15:36:58 -0300

    test: priemiro test ok reconhecendo caminhos mapeados em ts

--- src/pages/_App.test.tsx
+++ src/pages/_App.test.tsx
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom"
 import { render } from "@testing-library/react"
 import { describe, expect, test } from "vitest"
 
-import App from "@src/pages/_app"
+import App from "@src/pages/_App"
 
 describe("App", () => {
   test("deve renderizar o App com o texto: Alo mundo", () => {
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@
 
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     globals: true,
     environment: "jsdom",
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["node_modules", "dist", ".idea", ".git", ".cache"]
   },
 })
